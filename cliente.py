728fac1fa9af29154baab2bf3e4a66204d1f11d7
Ajdjggkrkrdj <128745808+Ajdjggkrkrdj@users.noreply.github.com>
2023-10-21 17:49:39 -0400

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -13,7 +13,7 @@ class RUVSUpload:
 		self.headers = {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
 		}
-	def upload(self, path, chat):
+	def upload(self, path, chat, replies):
 		options ={ 
 		'url': f'{self.host}?P=UserLogin',
 		'method': 'POST',
